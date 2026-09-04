3f379b4f28542b14f5ebea8e73c03a6ba0e6d170
Madesh M <madeshdex10@gmail.com>
2026-09-04 07:46:53 +0530

    update a config filr

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/backend/:path*",
+        destination: "http://3.25.164.245:8000/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
