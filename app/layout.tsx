503abb4c8eea5642bb7c2aa4996f90e59dd2991b
Madesh M <madeshdex10@gmail.com>
2026-09-08 10:40:51 +0530

    add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "../context/ThemeContext";
 import { SettingsProvider } from "../context/SettingsContext";
 import { LanguageProvider } from "@/context/LanguageContext";
 import Providers from "./providers";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function RootLayout({
   children,
@@ -17,6 +18,7 @@ export default function RootLayout({
             <SettingsProvider>
               <LanguageProvider>
                 {children}
+                <Analytics />
               </LanguageProvider>
             </SettingsProvider>
           </ThemeProvider>
