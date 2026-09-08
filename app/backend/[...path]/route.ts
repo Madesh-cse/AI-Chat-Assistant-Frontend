994bd50fbf894beb0c71952204fe1f5c940c96bb
Madesh M <madeshdex10@gmail.com>
2026-09-08 10:22:54 +0530

    update a public url

--- app/backend/[...path]/route.ts
+++ app/backend/[...path]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const BACKEND_URL = "http://3.26.174.61:8000";
+const BACKEND_URL = "http://3.106.142.160:8000";
 
 type RouteContext = {
   params: Promise<{
